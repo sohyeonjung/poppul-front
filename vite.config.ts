b53ea409a80d833f196a7a9a53bcab6293fe1ad7
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-03-12 12:53:29 +0900

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
       },
     },
   },
